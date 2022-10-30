e31a036139aa865e4b38a1fef4a8182253c967ee
Mark White <markgewhite@gmail.com>
2022-10-30 11:57:10 +0000

    Second test: loss calcs and optimizer at each stage

--- modelGradientsAE.m
+++ modelGradientsAE.m
@@ -2,7 +2,7 @@
 % Function: modelGradients for an autoencoder (no adversarial design)
 % ***********************************************************************/
 
-function [  gradEnc, gradDec, lossRecon, dlZ, dlXHat ] = ...
+function [  gradEnc, gradDec, lossRecon, dlZ ] = ...
                         modelGradientsAE( ...
                                         dlnetEnc, ...
                                         dlnetDec, ...
@@ -15,29 +15,26 @@ dlZ = forward( dlnetEnc, dlX );
 % predict the fake image from the fake code
 dlXHat = forward( dlnetDec, dlZ );
 
-if fullCalc
+% calculate the reconstruction loss
+lossRecon = mean( mean( 0.5*(dlXHat - dlX).^2, 1 ) );
 
-    % calculate the reconstruction loss
-    lossRecon = mean( mean( 0.5*(dlXHat - dlX).^2, 1 ) );
+if fullCalc
 
     % calculate the KL-divergence
     lossKL = klDivergence( dlZ );
 
-    % combine 
-    loss = 0.9*lossRecon + 0.1*lossKL;
-    
-    % calculate the gradients (following igul222)
-    [ gradEnc, gradDec ] = dlgradient( loss, ...
-                                       dlnetEnc.Learnables, ...
-                                       dlnetDec.Learnables, ...
-                                       'RetainData', true );
-
 else
 
-    lossRecon = 0;
-    gradEnc = [];
-    gradDec = [];
+    lossKL = 0;
 end
 
+% combine 
+loss = 0.9*lossRecon + 0.1*lossKL;
+
+% calculate the gradients (following igul222)
+[ gradEnc, gradDec ] = dlgradient( loss, ...
+                                   dlnetEnc.Learnables, ...
+                                   dlnetDec.Learnables, ...
+                                   'RetainData', true );
 
 end
